2dcf75cbf7d5bb32e03323e2bdf2f20a6d7508a9
Tom Howe <turtlebender@gmail.com>
2012-06-26 11:35:19 -0500

    Inital descriptor including dependencies

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as reqs:
+    install_requires = [line for line in reqs]
+
+CONFIG = {
+  'description':'client for GlobusOnline Nexus',
+  'version':'0.0.1',
+  'name':'nexus-client',
+  'package_dir': {'':'lib'},
+  'packages': [ 'nexus', ],
+  'install_requires': install_requires,
+  'test_requires': ['nose']
+}
+
+setup(**CONFIG)
