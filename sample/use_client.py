ec937495ede12575e5b24d54d16438453b37a8ff
Tom Howe <turtlebender@gmail.com>
2012-07-12 06:10:30 -0500

    readding sample

---
+++ sample/use_client.py
@@ -0,0 +1,25 @@
+import os.path
+from nexus import Client
+
+# First instantiate a client object either with a dictionary or with a yaml file
+pwd = os.path.dirname(__file__)
+client = Client(config_file=os.path.join(pwd, 'sample.yml'))
+# Generate a url for the end user to use to authorize this client/authenticate.
+url = client.generate_request_url()
+print "Please authenticate using the following url"
+print url
+token = raw_input("Please copy the resulting code here: ")
+# At this point the end user needs to authenticate with the supplied url.  The
+# easiest way to do this is: curl -k --user test:test1 "<supplied_url>".  The
+# result will contain the token in the "code" field.  Paste that here.
+
+# Validate the token:
+user = client.authenticate_user(token)
+if user is not None:
+    print "Yup, you are {0}".format(user)
+else:
+    print "That is not a valid authorization code"
+
+
+#Get an access key for yourself using rsa:
+print client.request_client_credential()
