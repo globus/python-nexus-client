0765dcc48a16d3b58a20c5c68e3fbff4c735f752
Tom Howe <turtlebender@gmail.com>
2012-07-11 21:36:54 -0500

    add utils
    This includes utilities for encoding strings and using openssh files for
    signatures

---
+++ lib/nexus/utils.py
@@ -0,0 +1,95 @@
+"""
+Various utilities used throughout.
+"""
+import base64
+import datetime
+import hashlib
+import os.path
+import struct
+from subprocess import Popen, PIPE
+import sys
+
+from pyasn1.type import univ
+from pyasn1.codec.der import encoder as der_encoder
+import rsa
+
+def b64encode(value):
+    """
+    Break things up into 60-character chunks.
+    """
+    b64 = base64.b64encode(value)
+    for i in xrange(0, len(b64), 60):
+        yield b64[i:i+60]
+
+def sha1_base64(value):
+    """
+    hash and encode a value
+    """
+    return b64encode(hashlib.sha1(value).digest())
+
+def canonical_time(timestamp):
+    """
+    Create a canonical time format.
+    """
+    if timestamp.tzinfo is not None:
+        timestamp = timestamp.astimezone(utc).replace(tzinfo=None)
+    return timestamp.replace(microsecond=0).isoformat() + 'Z'
+
+class UTC(datetime.tzinfo):
+    """
+    UTC timezone stub.
+    """
+
+    ZERO = datetime.timedelta(0)
+
+    def utcoffset(self, dt):
+        return self.ZERO
+
+    def tzname(self, dt):
+        return 'UTC'
+
+    def dst(self, dt):
+        return self.ZERO
+
+utc = UTC()
+
+def read_openssh_public_key(key_file):
+    """
+    Convert an openssl formatted key into a pkcs#1 formatted key as used by our
+    rsa library
+    """
+    with open(os.path.expanduser(key_file), 'r') as file_handle:
+        keydata = base64.b64decode(file_handle.read().split(None)[1])
+
+    parts = []
+    while keydata:
+        # read the length of the data
+        dlen = struct.unpack('>I', keydata[:4])[0]
+        # read in <length> bytes
+        data, keydata = keydata[4:dlen+4], keydata[4+dlen:]
+        parts.append(data)
+    e_val_list = ['%02X' % struct.unpack('B', x)[0] for x in parts[1]]
+    e_val = eval('0x' + ''.join(e_val_list))
+    n_val_list = ['%02X' % struct.unpack('B', x)[0] for x in parts[2]]
+    n_val = eval('0x' + ''.join(n_val_list))
+    pkcs1_seq = univ.Sequence()
+    pkcs1_seq.setComponentByPosition(0, univ.Integer(n_val))
+    pkcs1_seq.setComponentByPosition(1, univ.Integer(e_val))
+    encoded_string = base64.encodestring(der_encoder.encode(pkcs1_seq))
+    key_data = ('-----BEGIN RSA PUBLIC KEY-----\n'
+                '{0}\n'
+                '-----END RSA PUBLIC KEY-----').format(encoded_string)
+    return rsa.PublicKey.load_pkcs1(key_data)
+
+def read_openssh_private_key(key_file, password=None):
+    """
+    Given an openssh private key, return an rsa PrivateKey object
+    """
+    with open(os.path.expanduser(key_file), 'r') as file_handle:
+        key_data = file_handle.read()
+    if 'ENCRYPTED' in key_data:
+        command = 'openssl rsa -passin pass:{0} -in {1}'
+        proc = Popen(command.format(password, os.path.expanduser(key_file)),
+                stdout=PIPE, stderr=PIPE, shell=True)
+        key_data = proc.communicate()[0]
+    return rsa.PrivateKey.load_pkcs1(key_data)
