1e4cd29c470268a4d9c3aed858f101b9ec88fdf2
Mattias Lidman <mattias.lidman@gmail.com>
2012-09-13 18:29:22 +0200

    Added doc for use_client.py.
    [#GRAPH-923 transition:31]

--- sample/use_client.py
+++ sample/use_client.py
@@ -2,6 +2,15 @@ import os.path
 from getpass import getpass
 from nexus import Client
 
+# This sample makes the following assumptions about the items listed in the
+# sample.yml config file:
+# 1) 'client' exists as a user on the server and can be authenticated using the
+# password 'client_secret'.
+# 2) You may authenticate as any user, as long as the user has registered the
+# public part of whatever private key is listed as the 'user_private_key_file'.
+# By default the dummy_key in this sample directory is used, so the dummy_key.pub
+# must be registered with the users account.
+
 # First instantiate a client object either with a dictionary or with a yaml file
 pwd = os.path.dirname(__file__)
 client = Client(config_file=os.path.join(pwd, 'sample.yml'))
